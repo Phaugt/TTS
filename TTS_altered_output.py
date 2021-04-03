c9de0dd67b0ba65d0c46a7a53e924c066216012f
Patrik Hauguth <patrik.hauguth@gmail.com>
2021-04-03 11:48:41 +0200

    more voices on apple device

--- TTS_altered_output.py
+++ TTS_altered_output.py
@@ -2,16 +2,15 @@ import pyttsx3
 
 #init the TTS
 TTS = pyttsx3.init()
-TTS.setProperty('rate', 150)
+TTS.setProperty('rate', 175)
 
 #alter the voice output
-voices = TTS.getProperty('voices')       #get info about the voices
-#TTS.setProperty('voice', voices[0].id)  # 0 for male
-#TTS.setProperty('voice', voices[1].id)   # 1 for female
+voices = TTS.getProperty('voices')      #get info about the voices
+TTS.setProperty('voice', voices[3].id)   # 1 for female
 
 #string to speak
-#text = "pythonexplainedto.me"
-#TTS.save_to_file(text,"male_"+text+".mp3")
+text = "pythonexplainedto.me"
+TTS.save_to_file(text,"male_"+text+".mp3")
 
 #run the TTS
-#TTS.runAndWait()
+TTS.runAndWait()
